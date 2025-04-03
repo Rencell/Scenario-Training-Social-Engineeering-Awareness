7b27e66ef1950e69a1a3633aa8dfafdc1e63c957
rencell <rencelltobelonia67@gmail.com>
2025-04-03 18:19:46 +0800

    2025-03-04 06:19PM

--- Frontend/vue-frontend/src/components/Phishing/levelTwo/QuizTwo/quiz.vue
+++ Frontend/vue-frontend/src/components/Phishing/levelTwo/QuizTwo/quiz.vue
@@ -13,86 +13,125 @@
             <timer :key="dataPage.id" :timerout="nextData"></timer>
           </div>
         </div>
-        <div class="flex flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
-          <div
-            class="font-secondary flex justify-between text-sm font-semibold"
-          >
-            <p>Found: {{ found_count }} of {{ number_areas }}</p>
-            <p>{{ parseInt(found_percentage) }}%</p>
+        
+        <div class="w-full rounded-xl flex gap-4">
+
+          <div v-if="zoom == 'zoom'">
+            <ZoomImg class="rounded-xl bg-red-400 object-cover " :src="dataPage.image" />
           </div>
 
-          <progress
-            class="progress progress-success"
-            :value="found_percentage"
-            max="100"
-          ></progress>
-        </div>
-        <div class="w-full rounded-xl">
-          <img
-            class="rounded-xl bg-red-400 object-cover "
-            :src="dataPage.image"
-            :usemap="'#' + dataPage.id"
-            @click="score -= 5"
-          />
-
-          <map :name="'#' + dataPage.id">
-            <area
-              v-for="(target, key) in dataPage.areas"
-              :key="key"
-              :title="target.title"
-              @click="checkAreas(key)"
-              :coords="target.coordinates"
-              shape="poly"
-              class="cursor-pointer"
+          <div v-else :key="dataPage.id">
+            <img
+              class="rounded-xl bg-red-400 object-cover "
+              :src="dataPage.image"
+              :usemap="'#map' + dataPage.id"
+              @click="score -= 5"
             />
-          </map>
-        </div>
+  
+            <map :name="'#map' + dataPage.id" >
+              <area
+                v-for="(target, key) in dataPage.areas"
+                :key="target.id"
+                :title="target.id"
+                @click="checkAreas(key)"
+                :coords="target.coordinates"
+                shape="poly"
+                class="cursor-pointer"
+              />
+            </map>
+          </div>
 
+          <div class="grow flex flex-col gap-4 ">
+            <div class=" p-4 bg-primary rounded-md text-white flex-center flex-col">
+              <p>Found</p>
+              <p>{{ found_count }} of {{ number_areas }}</p>
+            </div>
+
+            <div class=" p-4 rounded-md  flex-center flex-col cursor-pointer" 
+            @click="zoom = 'zoom' " 
+            :class="zoom === 'zoom' ? 'bg-primary text-white' : 'bg-white text-primary'">
+              <i class="bi bi-zoom-in text-xl"></i>
+              <p class="text-xs font-secondary">Zoom</p>
+            </div>
+            <div class=" p-4 rounded-md  flex-center flex-col cursor-pointer" 
+            @click="zoom = 'select'" 
+            :class="zoom === 'select' ? 'bg-primary text-white' : 'bg-white text-primary'">
+              <i class="bi bi-cursor-fill text-xl"></i>
+              <p class="text-xs font-secondary">Select</p>
+            </div>
+
+
+          </div>
+        </div>
       </div>
 </template>
 
 <script setup>
-
+import { ZoomImg } from "vue3-zoomer";
 import imageMapResize from "image-map-resizer";
-import { computed, onMounted, ref, watch } from "vue";
+
+
+import { computed, nextTick, onMounted, ref, watch } from "vue";
 import { PhishingTwoQuizTwodata } from "@/composables/Phishing/levelTwo/QuizTwo";
 import timer from "@/components/Phishing/levelTwo/QuizTwo/timer.vue";
-const data = PhishingTwoQuizTwodata;
+const data = ref(PhishingTwoQuizTwodata);
 
 const pageCount = ref(0);
-const dataPage = computed(() => data[pageCount.value]);
+const dataPage = computed(() => data.value[pageCount.value]);
 const found_count = ref(0);
 const foundAreas = ref([]);
 const number_areas = ref(dataPage.value.areas.length);
-const found_percentage = computed(
-  () => (found_count.value / number_areas.value) * 100,
-);
+// const found_percentage = computed(
+//   () => (found_count.value / number_areas.value) * 100,
+// );
 const score = ref(0);
 
+const zoom = ref('select');
 const checkAreas = (area_id) => {
   if (foundAreas.value.includes(area_id)) return;
-
+  
   foundAreas.value.push(area_id);
   found_count.value++;
   score.value += 10;
 };
 
 const nextData = () => {
-  if (pageCount.value < data.length) pageCount.value++;
+  if (pageCount.value < data.value.length) pageCount.value++;
   found_count.value = 0;
   foundAreas.value = [];
+  imageMapResize();
 };
 
+
+const resizeMap = () => {
+  nextTick(() => {
+    imageMapResize();
+  });
+};
+
+watch(dataPage, async () => {
+  await nextTick();
+  resizeMap();
+})
+watch(zoom, async () => {
+  if(zoom.value === 'select'){
+
+    await nextTick();
+    resizeMap();
+  }
+})
+
 watch(found_count, () => {
-    if(found_count.value === number_areas.value){
-        if (pageCount.value < data.length - 1) {
-            nextData();
-        }
+  if(found_count.value === number_areas.value){
+    if (pageCount.value < data.value.length - 1) {
+      nextData();
     }
+  }
 })
 
 onMounted(() => {
   imageMapResize();
 });
+
 
 </script>
