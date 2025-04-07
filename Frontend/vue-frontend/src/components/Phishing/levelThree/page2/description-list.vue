ad63347c73b3eec0fb7ec5654f7fa7d86679f02b
rencell <rencell@gmail.com>
2025-04-08 04:31:54 +0800

    social media phishing 2

---
+++ Frontend/vue-frontend/src/components/Phishing/levelThree/page2/description-list.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    @click="showExample = !showExample"
+    class="flex flex-col gap-3 rounded-sm border-[0.5px] border-gray-200 bg-white duration-100 ease-in hover:scale-102 hover:shadow-lg pb-5"
+  >
+    <div @click="showExample = showExample" class="flex items-center justify-between p-5 py-8">
+      <div class="flex gap-4">
+        <div class="box-content w-10 rounded-full bg-gray-200 p-2">
+          <slot name="image"></slot>
+        </div>
+        <div class="flex flex-col gap-2">
+          <p class="font-secondary text-xl font-bold text-black">{{ heading }}</p>
+          <i class="text-sm text-gray-500">
+            {{ subheading }}
+          </i>
+        </div>
+      </div>
+
+      <div class="text-center">
+        <button
+          @click="showExample = showExample"
+          class="bg-primary-light rounded-full p-1 px-3 text-xs font-semibold text-[#487425] transition"
+        >
+          <div v-if="!showExample">
+            Expand<i class="bi bi-arrow-down-short"></i>
+          </div>
+          <div v-else>Collapse<i class="bi bi-arrow-up-short"></i></div>
+        </button>
+      </div>
+    </div>
+    <div
+      :class="{ hidden: !showExample }"
+      class="motion-preset-slide-down px-23"
+    >
+      <h1 class="font-secondary mb-4 text-xs font-semibold text-gray-400">
+        WHAT IS THIS?
+      </h1>
+      <p class="font-secondary mb-7 text-sm text-gray-800">
+        <slot name="description">
+          
+        </slot>
+      </p>
+
+      <h1 class="font-secondary mb-4 text-xs font-semibold text-gray-400">
+        SOME OF EXAMPLES
+      </h1>
+
+      <slot name="examples">
+
+      </slot>
+      <h1 class="font-secondary mb-4 text-xs font-semibold text-gray-400">
+        TYPICAL SIGNS OF {{ heading.toUpperCase() }}
+      </h1>
+     <div class="font-secondary ms-4">
+        <slot name="signs" >
+  
+        </slot>
+     </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const showExample = ref(false);
+
+const props = defineProps({
+  heading: String,
+  subheading: String,
+});
+</script>
--- Frontend/vue-frontend/src/views/Phishing/level_three/page2.vue
+++ Frontend/vue-frontend/src/views/Phishing/level_three/page2.vue
@@ -1,6 +1,88 @@
 <template>
-     <div class="h-full w-full bg-white-color rounded-xl overflow-hidden flex justify-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam itaque labore enim necessitatibus corporis, cumque fugiat facilis a iusto.
-     </div>
+  <div
+    class="bg-white-color h-full w-full flex justify-center overflow-auto rounded-xl"
+  >
+    <div class="w-4xl bg-white p-10 h-fit">
+      <div class="pb-10">
+        <p class="text-3xl text-black font-bold pb-2">Social Media Phishing</p>
+        <p class="text-gray-400 font-secondary">Despite all the benifits of social media, hackers take advantage over it</p>
+      </div>
+      <!-- Appeal to Curiosity -->
+      <descriptionList :heading="'Appeal to curiosity'" :subheading="'lorem2'">
+        <template v-slot:image>
+          <img src="/Images/Phishing/Page3/document.svg" alt="">
+        </template>
+        <template v-slot:description>
+          hello
+        </template>
+        <template v-slot:examples>
+        </template>
+        <template v-slot:signs>
+        </template>
+      </descriptionList>
+      <!-- 2. Fake accounts -->
+      <descriptionList :heading="'Fake Account Impersonation'" :subheading="'lorem2'">
+        <template v-slot:image>
+          <img src="/Images/Phishing/Page3/document.svg" alt="">
+        </template>
+        <template v-slot:description>
+          hello
+        </template>
+        <template v-slot:examples>
+        </template>
+        <template v-slot:signs>
+        </template>
+      </descriptionList>
+      <!-- 3. Malicious Links -->
+      <descriptionList :heading="'Malicious Links in Posts and Comments'" :subheading="'lorem2'">
+        <template v-slot:image>
+          <img src="/Images/Phishing/Page3/document.svg" alt="">
+        </template>
+        <template v-slot:description>
+          hello
+        </template>
+        <template v-slot:examples>
+        </template>
+        <template v-slot:signs>
+        </template>
+      </descriptionList>
+      <!-- 4. Comments Control -->
+      <descriptionList :heading="'Comments Control'" :subheading="'lorem2'">
+        <template v-slot:image>
+          <img src="/Images/Phishing/Page3/document.svg" alt="">
+        </template>
+        <template v-slot:description>
+          hello
+        </template>
+        <template v-slot:examples>
+        </template>
+        <template v-slot:signs>
+        </template>
+      </descriptionList>
+      <!-- 5. Information Stealing -->
+      <descriptionList :heading="'Quizzes and Surveys'" :subheading="'lorem2'">
+        <template v-slot:image>
+          <img src="/Images/Phishing/Page3/document.svg" alt="">
+        </template>
+        <template v-slot:description>
+          hello
+        </template>
+        <template v-slot:examples>
+        </template>
+        <template v-slot:signs>
+        </template>
+      </descriptionList>
 
+      <div class="flex justify-end pt-3">
+        <BaseButton :click="componentPage.nextPage">Next</BaseButton>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { usePhishingThree } from '@/store/Phishing/phishingLevelThree/componentPage';
+import BaseButton from '@/components/UI/BaseButton.vue';
+import descriptionList from '@/components/Phishing/levelThree/page2/description-list.vue';
+const componentPage = usePhishingThree();
+</script>
